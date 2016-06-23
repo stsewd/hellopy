57b35a150b3f608ef275bf84a90b93917078a29f
Santos Gallegos <santos_g@outlook.com>
2016-06-23 01:44:28 -0500

    main loop

--- hellopy/hellopy.py
+++ hellopy/hellopy.py
@@ -3,9 +3,10 @@ from . import speech_to_text as stt
 
 
 def main():
-    try:
-        msg = stt.listen()
-        print("HelloPy: " + msg)
-        bot.converse(msg)
-    except:
-        print("Error")
+    while True:
+        try:
+            msg = stt.listen()
+            print("HelloPy: " + msg)
+            bot.converse(msg)
+        except:
+            print("Error")
